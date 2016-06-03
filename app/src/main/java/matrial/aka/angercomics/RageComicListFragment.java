a609763a7ae23bad774358e08aa82cca7935a31d
syntaxSizer <kakuma4never@gmail.com>
2016-06-04 02:09:22 +0300

    add project files

---
+++ app/src/main/java/matrial/aka/angercomics/RageComicListFragment.java
@@ -0,0 +1,20 @@
+package matrial.aka.angercomics;
+
+import android.app.Fragment;
+
+/**
+ * Created by geckozila on 04/06/16.
+ */
+public class RageComicListFragment extends Fragment {
+    private int[] mImageResdIds;
+    private String [] mName;
+    private String[] descriptions;
+    private String [] mUrl;
+
+
+    public static RageComicListFragment newInstance(){
+        return new RageComicListFragment();
+    }
+    class RageComicAdapter extends R
+    }
+}
