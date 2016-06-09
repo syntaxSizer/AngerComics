2536c06c5da13f422c1759e2f2a15b867f82c581
syntaxSizer <kakuma4never@gmail.com>
2016-06-09 16:45:08 +0300

    add text and image resources

--- app/src/main/java/matrial/aka/angercomics/Fragmentne.java
+++ app/src/main/java/matrial/aka/angercomics/Fragmentne.java
@@ -2,48 +2,50 @@ package matrial.aka.angercomics;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by geckozila on 04/06/16.
  */
 public class Fragmentne extends Fragment {
 
-    private int [] mImageResId;
+    private int[] mImageResId;
     private String[] mName;
     private String[] mDescription;
     private String[] mUrl;
 
-    public static Fragmentne newInstace(){
+    public static Fragmentne newInstace() {
         return new Fragmentne();
     }
+
     // empty constructor
-    public Fragmentne(){
+    public Fragmentne() {
 
     }
 
-    class FragmentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    class FragmentAdapter extends RecyclerView.Adapter<ViewHolder> {
 
         private LayoutInflater mLayoutInflater;
 
-        public FragmentAdapter(Context contect){
+        public FragmentAdapter(Context contect) {
             mLayoutInflater = LayoutInflater.from(contect);
         }
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ViewHolder(mLayoutInflater.inflate(R.layout.recycler_item_rage_comic,parent,false));
+            return new ViewHolder(mLayoutInflater.inflate(R.layout.recycler_item_rage_comic, parent, false));
 
 
         }
 
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-         final int imageResId = mImageResId[position];
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            final int imageResId = mImageResId[position];
             final String name = mName[position];
             final String descriptions = mDescription[position];
             final String url = mUrl[position];
@@ -54,22 +56,25 @@ public class Fragmentne extends Fragment {
         public int getItemCount() {
             return mName.length;
         }
-        class ViewHolder extends RecyclerView.ViewHolder{
+    }
+        class ViewHolder extends RecyclerView.ViewHolder {
 
-            public ViewHolder(View inflate) {
-                super(inflate);
+
+            private ImageView mimageview;
+            private TextView mNameTextview;
+
+            public ViewHolder(View itemview) {
+                super(itemview);
+                //get reference to  image and name
+                mimageview = (ImageView) itemview.findViewById(R.id.comic_image);
+                mNameTextview = (TextView) itemview.findViewById(R.id.name);
+
             }
+
+            private void setData(int imageResId, String name) {
+                mimageview.setImageResource(imageResId);
+                mNameTextview.setText(name);
+
         }
     }
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
-
-    }
-
-
 }
-
-
