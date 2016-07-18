6a7924feede942b8e797dd1201858b8236d0d7e5
syntaxSizer <kakuma4never@gmail.com>
2016-07-18 18:03:22 +0300

    add splash screen java code

--- app/src/main/java/matrial/aka/angercomics/Spalsh.java
+++ app/src/main/java/matrial/aka/angercomics/Spalsh.java
@@ -1,9 +1,42 @@
 package matrial.aka.angercomics;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 /**
  * Created by geckozila on 18/07/16.
  */
 public class Spalsh extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(Spalsh.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause () {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+
+
+    }
 }
+
+
